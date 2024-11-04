d6aa22b667ba0a61893500bc433aef8de2f20634
DavidVSantiago <david.valente.santiago@gmail.com>
2024-11-04 18:43:47 -0300

    'update'

--- _old/Modelo_03/Readme.txt
+++
@@ -1,3 +0,0 @@
-Lista de afazeres:
-
-- impleemntar atrasos aleatórios entre as consultas http, para simular usuário real
--- _old/Modelo_03/app.js
+++
@@ -1,9 +0,0 @@
-const FileUtils = require('./modulos/file_utils');
-const WordpressUtils = require('./modulos/wordpress_utils');
-
-(async () => {
-  const fileUtils = new FileUtils();
-  const wordpressUtils = new WordpressUtils();
-  // await fileUtils.removeCorruptedData('french.json');
-  await wordpressUtils.bulkUploadPosts('french.json','fr');
-})();
